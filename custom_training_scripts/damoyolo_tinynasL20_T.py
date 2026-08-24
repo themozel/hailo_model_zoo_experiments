16b5cfd7d41e374eab8d6173bdae3cf131650f58
themozel <amidmozelli@gmail.com>
2026-08-24 12:53:04 +0000

    adding all edited/created scripts locally

--- custom_training_scripts/damoyolo_tinynasL20_T.py
+++ custom_training_scripts/damoyolo_tinynasL20_T.py
@@ -14,7 +14,7 @@ class Config(MyConfig):
         self.miscs.eval_interval_epochs = 10
         self.miscs.ckpt_interval_epochs = 10
         # optimizer
-        self.train.batch_size = 96
+        self.train.batch_size = 64
         self.train.base_lr_per_img = 0.01 / 64
         self.train.min_lr_ratio = 0.05
         self.train.weight_decay = 5e-4
@@ -31,9 +31,12 @@ class Config(MyConfig):
         # Percept
         # self.train.augment.transform.image_max_range = (736, 992)
         # self.train.augment.mosaic_mixup.mosaic_size = (736, 992)   # <-- add this; default (640,640) is square
-        # Percept-cropped
-        self.train.augment.transform.image_max_range = (640, 640)
-        self.train.augment.mosaic_mixup.mosaic_size = (640, 640)   # <-- add this; default (640,640) is square
+        # # Percept-cropped
+        # self.train.augment.transform.image_max_range = (640, 640)
+        # self.train.augment.mosaic_mixup.mosaic_size = (640, 640)   # <-- add this; default (640,640) is square
+        # GERALD-cropped
+        self.train.augment.transform.image_max_range = (704, 704)
+        self.train.augment.mosaic_mixup.mosaic_size = (704, 704)   # <-- add this; default (640,640) is square
         self.train.augment.mosaic_mixup.mixup_prob = 1.0
         self.train.augment.mosaic_mixup.degrees = 10.0
         self.train.augment.mosaic_mixup.translate = 0.1
@@ -46,9 +49,12 @@ class Config(MyConfig):
         # Percept
         # self.dataset.train_ann = ('percept_coco_train', )
         # self.dataset.val_ann = ('percept_coco_val', )
-        # Percept-cropped
-        self.dataset.train_ann = ('percept_cropped_coco_train', )
-        self.dataset.val_ann = ('percept_cropped_coco_val', )
+        # # Percept-cropped
+        # self.dataset.train_ann = ('percept_cropped_coco_train', )
+        # self.dataset.val_ann = ('percept_cropped_coco_val', )
+        # GERALD-cropped
+        self.dataset.train_ann = ('gerald_cropped_coco_train', )
+        self.dataset.val_ann = ('gerald_cropped_coco_val', )
 
         # backbone
         structure = self.read_structure(
@@ -81,9 +87,9 @@ class Config(MyConfig):
         ZeroHead = {
             'name': 'ZeroHead',
             # GERALD
-            # 'num_classes': 60,
-            # Percept
-            'num_classes': 33,
+            'num_classes': 60,
+            # # Percept
+            # 'num_classes': 33,
             'in_channels': [64, 128, 256],
             'stacked_convs': 0,
             'reg_max': 16,
@@ -94,7 +100,7 @@ class Config(MyConfig):
         self.model.head = ZeroHead
 
         # GERALD (60 classes)
-        # self.dataset.class_names = ['El_6', 'Hectometer_Sign', 'Hp_0_HV', 'Hp_0_Ks', 'Hp_0_Sh', 'Hp_1', 'Hp_2', 'ICE', 'Ks_1', 'Ks_2', 'LZB', 'Lf_2', 'Lf_3', 'Lf_6', 'Lf_7', 'Mast_Sign_WRW', 'Mast_Sign_WYWYW', 'Mast_Sign_Y_Triangle', 'Ne_1', 'Ne_2', 'Ne_3_1', 'Ne_3_2', 'Ne_3_3', 'Ne_3_4', 'Ne_3_5', 'Ne_4', 'Ne_5', 'Ne_6', 'Ne_7a', 'Ne_7b', 'Platform_Display', 'Platform_Text_Sign', 'Platform_Track_Sign', 'Platform_Warn_Sign', 'Ra_10', 'Ride_Indicator_1', 'Ride_Indicator_Off', 'Sh_0', 'Sh_1', 'Sh_2', 'Sign_Back', 'Signal_Back', 'Signal_Identifier_Sign', 'Signal_Invalid', 'Signal_Off', 'So_20_Left', 'So_20_Right', 'Traffic_Light', 'Traffic_Sign', 'Vr_0', 'Vr_1', 'Vr_2', 'Wn_1', 'Wn_2', 'Zs_2', 'Zs_2v', 'Zs_3', 'Zs_3v', 'Zs_6', 'Zs_Off']
+        self.dataset.class_names = ['El_6', 'Hectometer_Sign', 'Hp_0_HV', 'Hp_0_Ks', 'Hp_0_Sh', 'Hp_1', 'Hp_2', 'ICE', 'Ks_1', 'Ks_2', 'LZB', 'Lf_2', 'Lf_3', 'Lf_6', 'Lf_7', 'Mast_Sign_WRW', 'Mast_Sign_WYWYW', 'Mast_Sign_Y_Triangle', 'Ne_1', 'Ne_2', 'Ne_3_1', 'Ne_3_2', 'Ne_3_3', 'Ne_3_4', 'Ne_3_5', 'Ne_4', 'Ne_5', 'Ne_6', 'Ne_7a', 'Ne_7b', 'Platform_Display', 'Platform_Text_Sign', 'Platform_Track_Sign', 'Platform_Warn_Sign', 'Ra_10', 'Ride_Indicator_1', 'Ride_Indicator_Off', 'Sh_0', 'Sh_1', 'Sh_2', 'Sign_Back', 'Signal_Back', 'Signal_Identifier_Sign', 'Signal_Invalid', 'Signal_Off', 'So_20_Left', 'So_20_Right', 'Traffic_Light', 'Traffic_Sign', 'Vr_0', 'Vr_1', 'Vr_2', 'Wn_1', 'Wn_2', 'Zs_2', 'Zs_2v', 'Zs_3', 'Zs_3v', 'Zs_6', 'Zs_Off']
 
         # Percept (33 classes)
-        self.dataset.class_names = ['obs_animal', 'obs_bus', 'obs_car', 'obs_cyclist', 'obs_motorcyclist', 'obs_person', 'obs_stroller', 'obs_tram', 'sig_free_left', 'sig_free_right', 'sig_free_straight', 'sig_max_curve_speed', 'sig_max_speed_12', 'sig_max_speed_18', 'sig_max_speed_24', 'sig_max_speed_30', 'sig_max_speed_36', 'sig_max_speed_42', 'sig_max_speed_48', 'sig_max_speed_60', 'sig_stop', 'sig_switch_left_free', 'sig_switch_left_locked', 'sig_switch_right_free', 'sig_switch_right_locked', 'sig_switch_straight_free', 'sig_switch_straight_locked', 'sw_cross_left', 'sw_cross_right', 'sw_forward_left', 'sw_forward_right', 'sw_reverse_left', 'sw_reverse_right', 'sw_unknown']
+        # self.dataset.class_names = ['obs_animal', 'obs_bus', 'obs_car', 'obs_cyclist', 'obs_motorcyclist', 'obs_person', 'obs_stroller', 'obs_tram', 'sig_free_left', 'sig_free_right', 'sig_free_straight', 'sig_max_curve_speed', 'sig_max_speed_12', 'sig_max_speed_18', 'sig_max_speed_24', 'sig_max_speed_30', 'sig_max_speed_36', 'sig_max_speed_42', 'sig_max_speed_48', 'sig_max_speed_60', 'sig_stop', 'sig_switch_left_free', 'sig_switch_left_locked', 'sig_switch_right_free', 'sig_switch_right_locked', 'sig_switch_straight_free', 'sig_switch_straight_locked', 'sw_cross_left', 'sw_cross_right', 'sw_forward_left', 'sw_forward_right', 'sw_reverse_left', 'sw_reverse_right', 'sw_unknown']
--- custom_training_scripts/yolox_x_leaky_zeus.py
+++ custom_training_scripts/yolox_x_leaky_zeus.py
@@ -40,13 +40,24 @@ class Exp(MyExp):
         # self.num_classes = 34  # Set this to the number of classes in your dataset
         # self.data_num_workers = 4
         
-        ### Percept-cropped dataset configuration
-        self.data_dir = "/data/percept-cropped/"
+        # ### Percept-cropped dataset configuration
+        # self.data_dir = "/data/percept-cropped/"
+        # self.train_ann = "instances_train2017.json"
+        # self.val_ann = "instances_val2017.json"
+        # self.test_ann = "instances_test2017.json"
+        # # Percept dataset images are 1280x736 (16:9) — use this pair instead if switching datasets
+        # self.input_size = (640, 640)  # (height, width); matches native 4:3 image aspect ratio, both dims multiples of 32
+        # self.test_size = (640, 640)
+        # self.num_classes = 34  # Set this to the number of classes in your dataset
+        # self.data_num_workers = 14
+        
+        ### GERALD dataset configuration
+        self.data_dir = "/data/GERALD-cropped/"
         self.train_ann = "instances_train2017.json"
         self.val_ann = "instances_val2017.json"
         self.test_ann = "instances_test2017.json"
-        # Percept dataset images are 1280x736 (16:9) — use this pair instead if switching datasets
-        self.input_size = (640, 640)  # (height, width); matches native 4:3 image aspect ratio, both dims multiples of 32
-        self.test_size = (640, 640)
-        self.num_classes = 34  # Set this to the number of classes in your dataset
-        self.data_num_workers = 14
+        # GERALD dataset images are 1920x1080 (16:9, some 1280x720) — use this pair instead if switching datasets
+        self.input_size = (704, 704)  # (height, width); matches 16:9 aspect ratio, both dims multiples of 32
+        self.test_size = (704, 704)
+        self.num_classes = 60  # Set this to the number of classes in your dataset
+        self.data_num_workers = 6
